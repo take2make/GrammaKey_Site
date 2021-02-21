1f47399d917d78bda9c9a68f50fb8f5ddc6c3e41
Aleksei <54644940+take2make@users.noreply.github.com>
2021-02-21 11:29:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -10,24 +10,36 @@ import (
 
 )
 
+// site data
 type FormData struct {
   Loaded bool
   Result string
 }
 
+// get response on Post request
 type RespponsePostData struct {
   Id int64         `json:"id"`
 }
 
+// get response on Get request from specific session 
 type RespponseGetData struct {
   Answer string   `json:"answer"`
 }
 
+// api_url on which site send Post data
 var api_url = "http://192.168.1.238:8080/"
 
 
 // Display the named template
 func display(w http.ResponseWriter, page string, is_load bool, result string) {
+  /*
+  Display specific template, which is differnt, because it has some time
+  to get Result text.
+  Input:
+  	page - displate this page
+	is_load - bool var which shows loaded data or not
+	result - if is_load equals true then result contains the result text from api_url
+  */
   t, err := template.ParseFiles("templates/index.html")
 
   if err != nil {
@@ -39,6 +51,15 @@ func display(w http.ResponseWriter, page string, is_load bool, result string) {
 }
 
 func post_data(url string, text string) (session_id int64) {
+  /* 
+  Post data on api_url for future processing on api side
+  Input:
+  	url - post data on this url
+	text - key sentece which is send to url
+  Output:
+  	session_id - int number which shows session id of processing
+	text data
+  */
   postBody, _ := json.Marshal(map[string]string{
     "text":text,
   })
@@ -53,14 +74,25 @@ func post_data(url string, text string) (session_id int64) {
 
   post_data := RespponsePostData{}
   json.NewDecoder(resp.Body).Decode(&post_data)
+
   return post_data.Id
 }
 
 func get_data_from_session(session int64) <- chan string {
+  /*
+  When site get sessing id, site needs to send get request to the
+  following url: api_url/<session_id> to get result data
+  Input:
+  	sessin - int number of session
+  Output:
+  	chan: channel that transfer data
+  */
   url_session := fmt.Sprintf("%s%d", api_url, session)
   fmt.Println(url_session)
 
   r := make(chan string)
+  // it creates lightweight thread, so the function
+  // get_data_from_session is actually asynchronous
   go func() {
     defer close(r)
     for {
@@ -84,6 +116,9 @@ func get_data_from_session(session int64) <- chan string {
 }
 
 func upload_text(w http.ResponseWriter, r *http.Request) {
+  /*
+  Upload text to site and then send to api_url
+  */
   key_sentence := r.FormValue("key_sentence")
 
   fmt.Println("key sentence = ", key_sentence)
@@ -98,6 +133,9 @@ func upload_text(w http.ResponseWriter, r *http.Request) {
 }
 
 func view_handler(w http.ResponseWriter, r *http.Request) {
+  /* 
+  Handler to view GET or POST data on site
+  */
   switch r.Method {
   	case "GET":
   		display(w, "index", false, "")
@@ -108,6 +146,6 @@ func view_handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", view_handler)
-  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+        http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.ListenAndServe(":8000", nil)
 }
